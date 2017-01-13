96026810ab0f76fdc59d5fb3655bd273ddb90040
Andy Ye <Andy.Ye@rock-chips.com>
2017-01-13 14:16:37 +0800

    fix NPE during monkey test
    // java.lang.NullPointerException: Attempt to invoke virtual method 'com.android
    .gallery3d.filtershow.history.HistoryManager com.android.gallery3d.filtershow.im
    ageshow.MasterImage.getHistory()' on a null object reference
    // at com.android.gallery3d.filtershow.FilterShowActivity.resetHistory(Filt
    erShowActivity.java:1293)
    // at com.android.gallery3d.filtershow.FilterShowActivity.onOptionsItemSele
    cted(FilterShowActivity.java:1054)
    // at android.app.Activity.onMenuItemSelected(Activity.java:3231)
    
    Change-Id: I2579ef2f47ef496e8a4a6410e894b6e4fd1d2b72

--- src/com/android/gallery3d/filtershow/FilterShowActivity.java
+++ src/com/android/gallery3d/filtershow/FilterShowActivity.java
@@ -1290,6 +1290,9 @@ public class FilterShowActivity extends FragmentActivity implements OnItemClickL
     }
 
     void resetHistory() {
+        if(mMasterImage == null){
+            return;
+        }
         HistoryManager adapter = mMasterImage.getHistory();
         adapter.reset();
         HistoryItem historyItem = adapter.getItem(0);
